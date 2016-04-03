0b7f681ee444dd651a1d56ce035de87e146518a1
nathanhleung <nathanhleung@users.noreply.github.com>
2016-04-03 10:20:33 -0400

    install pip

--- deploy.cmd
+++ deploy.cmd
@@ -1,1 +1,1 @@
-sudo apt-get install python-pip && pip install -r requirements.txt && npm install && node app
+python -v && setx PATH "%PATH%;C:\Python27\Scripts" && pip install -r requirements.txt && npm install && node app
