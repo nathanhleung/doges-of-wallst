692322cced5fcd2496067b1aef0497aea9f679c1
nathanhleung <nathanhleung@users.noreply.github.com>
2016-04-03 05:41:26 -0400

    almost done

--- controllers/main.js
+++ controllers/main.js
@@ -4,3 +4,20 @@ exports.home = (req, res) => {
     page: 'home',
   });
 };
+
+exports.algo = (req, res) => {
+  res.render('algo', {
+    title: 'Algorithm Console: Doges of Wall Street',
+    page: 'algo',
+  });
+};
+
+exports.postAlgo = (req, res) => {
+  req.assert('sma-threshold', 'Percent value for SMA is not a number').isNumber();
+  req.assert('seed', 'Starting portfolio is not a valid amount').isNumber();
+  
+  res.send(req.body);
+};
+
+
+
